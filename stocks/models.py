61d0aa8a3fd950d1798a9f4138ecd2590628ef25
Shubham Mishra <mishrashubham577@gmail.com>
2018-09-01 01:09:34 +0530

    Add files via upload

---
+++ stocks/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Stock(models.Model):
+	ticker  = models.CharField(max_length=10)
+	Open = models.FloatField()
+	close = models.FloatField()
+	volume = models.IntegerField()
+
+	def __str__(self):
+		return self.ticker
